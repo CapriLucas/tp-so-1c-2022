99cb1c50441bd22f59924eab5d3b36f86be675a7
CapriLucas <capriatalucas@gmail.com>
2022-04-30 21:31:39 -0300

    inicializacion consola

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -1,7 +1,23 @@
 #include "consola.h"
 
+t_log* mainLog;
+t_config_consola* mainConfig;
+
+static void initializeProcess(){
+    mainLog = log_create("./logs/consola.log", "CONSOLA", true, LOG_LEVEL_INFO);
+    mainConfig = malloc(sizeof(t_config_consola));
+    if (!cargar_configuracion(mainConfig, mainLog)){
+        exit(EXIT_FAILURE);
+    }
+}
+
 int main(){
-    t_log* logger = log_create("./logs/consola.log", "CONSOLA", true, LOG_LEVEL_INFO);
-    log_info(logger, "Soy el proceso consola! %s", mi_funcion_compartida());
-    log_destroy(logger);
+    initializeProcess();
+
+    int kernelFd;
+    if(!generar_conexiones(kernelFd, mainConfig, mainLog)){
+        cerrar_programa(mainConfig, mainLog);
+        return EXIT_FAILURE;
+    }
+    cerrar_programa(mainConfig, mainLog);
 }
