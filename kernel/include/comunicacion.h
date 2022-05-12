8c0596b61557974cf50ba86e78a83a4b5d2bb4b8
CapriLucas <capriatalucas@gmail.com>
2022-05-11 21:00:58 -0300

    faet: add basic server config for recv multiple console clients at the same time

---
+++ kernel/include/comunicacion.h
@@ -0,0 +1,17 @@
+#ifndef COMUNICACION_KERNEL_H_
+#define COMUNICACION_KERNEL_H_
+
+#include <stdint.h>
+#include <string.h>
+#include <pthread.h>
+#include <commons/log.h>
+#include <commons/config.h>
+
+#include "shared_utils.h"
+#include "sockets.h"
+#include "protocolo.h"
+
+
+int server_escuchar(char* server_name, int server_socket, t_log* logger);
+
+#endif
---
+++ kernel/src/init_kernel.c
@@ -0,0 +1,74 @@
+#include "init_kernel.h"
+
+void cerrar_programa(t_config_kernel* cfg, t_log* mainLog, int* server_fd) {
+    log_destroy(mainLog);
+    liberar_conexion(server_fd);
+    
+    free(cfg->IP_MEMORIA);
+    free(cfg->IP_CPU);
+    free(cfg->ALGORITMO_PLANIFICACION);
+    free(cfg);
+
+    rl_clear_history();
+}
+
+bool crear_servidor(t_config_kernel* cfg, t_log* logger, int* fd, char* name) {
+    char* puerto = string_itoa(cfg->PUERTO_ESCUCHA);
+    *fd = iniciar_servidor(
+            logger,
+            name,
+            "0.0.0.0",
+            puerto
+    );
+    free(puerto);
+    if (*fd != -1) log_info(logger, "Server listo en KERNEL");
+    return *fd != -1;
+}
+
+uint8_t cargar_configuracion(t_config_kernel* config, t_log* mainLog) {
+    t_config* cfg = config_create("./cfg/kernel.config");
+
+    if(cfg == NULL) {
+        log_error(mainLog, "No se encontro kernel.config");
+        return 0;
+    }
+
+    char* properties[] = {
+        "PUERTO_ESCUCHA",    
+        "IP_MEMORIA",
+        "IP_CPU",
+        "PUERTO_MEMORIA",
+        "PUERTO_CPU_DISPATCH",
+        "PUERTO_CPU_INTERRUPT",
+        "ALGORITMO_PLANIFICACION",
+        "ESTIMACION_INICIAL",    
+        "ALFA",
+        "GRADO_MULTIPROGRAMACION",    
+        "TIEMPO_MAXIMO_BLOQUEADO",
+        NULL
+    };
+
+    if(!config_has_all_properties(cfg, properties)) {
+        log_error(mainLog, "Propiedades faltantes en el archivo de configuracion");
+        config_destroy(cfg);
+        return 0;
+    }
+
+    config->PUERTO_ESCUCHA = config_get_int_value(cfg,"PUERTO_ESCUCHA");
+    config->IP_MEMORIA = strdup(config_get_string_value(cfg, "IP_MEMORIA"));
+    config->IP_CPU = strdup(config_get_string_value(cfg, "IP_CPU"));
+    config->PUERTO_MEMORIA = config_get_int_value(cfg,"PUERTO_MEMORIA");    
+    config->PUERTO_CPU_DISPATCH = config_get_int_value(cfg,"PUERTO_CPU_DISPATCH");    
+    config->PUERTO_CPU_INTERRUPT = config_get_int_value(cfg,"PUERTO_CPU_INTERRUPT");    
+    config->ALGORITMO_PLANIFICACION = strdup(config_get_string_value(cfg, "ALGORITMO_PLANIFICACION"));
+    config->ESTIMACION_INICIAL = config_get_int_value(cfg,"ESTIMACION_INICIAL");    
+    config->ALFA = config_get_double_value(cfg,"ALFA");    
+    config->GRADO_MULTIPROGRAMACION = config_get_int_value(cfg,"GRADO_MULTIPROGRAMACION");    
+    config->TIEMPO_MAXIMO_BLOQUEADO = config_get_int_value(cfg,"TIEMPO_MAXIMO_BLOQUEADO");    
+
+    log_info(mainLog, "Archivo de configuracion cargado correctamente");
+
+    config_destroy(cfg);
+
+    return 1;
+}
