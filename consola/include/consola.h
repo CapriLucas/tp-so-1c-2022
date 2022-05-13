8f3232dbf969b96ee3171e89a3156b7030561609
xyz-frba <afraile@frba.utn.edu.ar>
2022-05-13 13:55:16 -0300

    2-consola-agregar-obtencíon-por-argc-y-argv-de-path-de-pseudocodigo-y-tamaño-proceso

--- consola/include/consola.h
+++ consola/include/consola.h
@@ -2,8 +2,19 @@
 #define CONSOLA_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 #include <commons/log.h>
 #include <stdbool.h>
 #include "shared_utils.h"
+#include <dirent.h>
+#include <errno.h>
+
+
+typedef struct {
+    char* PATH;
+    uint32_t MEMORY_SIZE;
+} t_console_cmd;
 
 #endif
--- consola/src/consola.c
+++ consola/src/consola.c
@@ -1,7 +1,68 @@
 #include "consola.h"
 
-int main(){
+
+int console_cmd(t_console_cmd* cmd, char* argv1, char* argv2, t_log* logger) {
+
+    // Test path: argv[1]
+    cmd->PATH = strdup(argv1);
+    DIR* dir = opendir(cmd->PATH);
+    if (dir) {                      // Path exists.
+        closedir(dir);                     
+    }
+    else if (ENOENT == errno) {     // Path does not exists.
+        log_error(logger, "Path %s does not exists.\n", argv1); 
+        printf("Path does not exists.\n");
+        return EXIT_FAILURE;
+    }
+    else {                          // Path failed for some reason.
+        log_error(logger, "Path %s could not be open.\n", argv1); 
+        printf("Path could not be open.\n");      
+        return EXIT_FAILURE;
+    }
+
+    // Memory size: argv[2]
+    if ( (cmd->MEMORY_SIZE = atoi(argv2)) == 0 ) {     // ToDO: validar min & max memory size.
+        log_error(logger, "Parámetro memoria inválido: %s\n", argv2); 
+        printf("Invalid memory size.\n");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+
+}
+
+
+int main(int argc, char** argv) {
+
+    // Test argc 
+    if (argc < 3) {
+        printf("Usage: console <path> <memory size>\n");        
+        return EXIT_FAILURE;
+    }
+    
     t_log* logger = log_create("./logs/consola.log", "CONSOLA", true, LOG_LEVEL_INFO);
-    log_info(logger, "Soy el proceso consola! %s", mi_funcion_compartida());
+
+    t_console_cmd* cmd = malloc(sizeof(t_console_cmd));
+
+
+    if (console_cmd(cmd, argv[1], argv[2], logger) > 0) {
+        log_error(logger, "Error al ejecutar console_cmd.\n"); 
+        printf("Error al ejecutar console_cmd.\n");
+        log_destroy(logger);
+        free(cmd);
+        return EXIT_FAILURE;
+    }
+
+
+    // Test printf
+    log_info(logger, "Path %s and memory size %d.", cmd->PATH, cmd->MEMORY_SIZE);
+    printf("Path: %s\n", cmd->PATH);
+    printf("Memory size: %d\n", cmd->MEMORY_SIZE);
+    
+
+    // Clean up
     log_destroy(logger);
+    free(cmd);
+
+    return EXIT_SUCCESS;
 }
